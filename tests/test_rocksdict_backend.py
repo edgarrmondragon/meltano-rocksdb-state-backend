a41a802116eaeb225c1336aac4ed14d780c546bd
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2023-11-19 12:40:32 -0600

    test: Fix Windows tests (#3)

--- tests/test_rocksdict_backend.py
+++ tests/test_rocksdict_backend.py
@@ -1,18 +1,32 @@
 from __future__ import annotations
 
+import shutil
 from pathlib import Path
 
+import pytest
 from meltano.core.job_state import JobState
 from meltano.core.project import Project
+from meltano.core.setting_definition import SettingKind
 from meltano.core.state_store import state_store_manager_from_project_settings
 
 from meltano_rocksdb_state_backend.rocksdb import RocksDBStateStoreManager
 
 
+@pytest.fixture()
+def project(tmp_path: Path) -> Project:
+    path = tmp_path / "project"
+    shutil.copytree(
+        "fixtures/project",
+        path,
+        ignore=shutil.ignore_patterns(".meltano/**"),
+    )
+    return Project.find(path.resolve())
+
+
 def test_state_store(tmp_path: Path) -> None:
     path = tmp_path / "test_dict"
     manager = RocksDBStateStoreManager(
-        uri=f"rocksdb://{path.resolve()}",
+        uri=f"rocksdb://{path.resolve().as_posix()}",
         write_buffer_size=None,
     )
 
@@ -63,29 +77,36 @@ def test_state_store(tmp_path: Path) -> None:
     assert manager.get_state_ids() == []
 
 
-def test_write_buffer_size(tmp_path: Path) -> None:
-    path = tmp_path / "test_dict"
-    RocksDBStateStoreManager(
-        uri=f"rocksdb://{path.resolve()}",
-        write_buffer_size=1024,
-    )
+def test_get_manager(project: Project) -> None:
+    project.settings.set("state_backend.rocksdb.write_buffer_size", 0x2000000)
 
-
-def test_get_manager() -> None:
-    project = Project.find("fixtures/project")
     manager = state_store_manager_from_project_settings(project.settings)
 
     assert isinstance(manager, RocksDBStateStoreManager)
-    assert manager.parsed.scheme == "rocksdb"
-    assert manager.parsed.path.endswith("meltano/state")
+    assert manager.scheme == "rocksdb"
+    assert manager.db.path().endswith(".meltano/state")
     assert manager.write_buffer_size == 0x2000000
 
-    write_buffer_size = project.settings.find_setting(
-        "state_backend.rocksdb.write_buffer_size",
+    manager.set(
+        JobState(
+            state_id="test",
+            updated_at=None,
+            partial_state={},
+            completed_state={"key": "value"},
+        ),
     )
-    write_buffer_size_env_vars = [
-        v.key for v in write_buffer_size.env_vars(prefixes=["meltano"])
-    ]
-    assert write_buffer_size_env_vars == [
-        "MELTANO_STATE_BACKEND_ROCKSDB_WRITE_BUFFER_SIZE",
-    ]
+    sys_dir_root = project.sys_dir_root
+    assert sys_dir_root.joinpath("state", "rocksdict-config.json").exists()
+
+
+def test_settings(project: Project) -> None:
+    setting_name = "state_backend.rocksdb.write_buffer_size"
+    project.settings.set(setting_name, 0x2000000)
+
+    write_buffer_size = project.settings.find_setting(setting_name)
+
+    assert write_buffer_size.label == "Writer Buffer Size"
+    assert write_buffer_size.kind == SettingKind.INTEGER
+
+    env_vars = write_buffer_size.env_vars(prefixes=["meltano"])
+    assert env_vars[0].key == "MELTANO_STATE_BACKEND_ROCKSDB_WRITE_BUFFER_SIZE"
